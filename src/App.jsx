ca5143ef85053876a50700a119a9baf8a7e8ea53
davidkhalid <khaliddavid952@gmail.com>
2024-11-02 01:19:51 +0700

    Define todos state and show them in the browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,41 @@
 // import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [todos] = useState([
+    {
+      id: 1,
+      title: "Finis Progate React Course",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Have lunch with Guru Domba",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Study React with Ninja Ken",
+      completed: false,
+    },
+  ]);
+
+  console.log(todos);
 
   return (
     <>
-    <div>
-      <h1>Hello React</h1>
-    </div>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>
+        <h1>React Project</h1>
+        {todos.map((todo) => {
+          return <p key={todo.id}>{todo.title}</p>;
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
